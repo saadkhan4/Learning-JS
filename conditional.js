18569a2374c2b259e3545be7591bebe20532bf21
saadkhan4 <saadshaheen1327@gmail.com>
2025-04-29 22:01:02 +0500

    learned conditional statements

---
+++ conditional.js
@@ -0,0 +1,50 @@
+// Conditional statements in JS.
+
+//1. if statement
+//2. if else statement
+//3. if else if else statement
+//4. switch statement
+
+// IF STATEMENT:-
+let age = 18;
+
+if (age >= 18) {
+    console.log("You can vote. ")
+}
+
+// IF ELSE STATEMENT:-
+let marks = 90;
+
+if (marks >= 50) {
+    console.log("You are passed! ");
+} else {
+    console.log("You are failed! ");
+}
+
+// IF ELSE IF ELSE STATEMENT:-
+let temperature = 35;
+
+if (temperature >= 40) {
+    console.log("It's very hot outside. ")
+} else if (temperature >= 25) {
+    console.log("The weather is warm. ")
+} else {
+    console.log("It's cool or cold outside. ")
+}
+
+// SWITCH STATEMENT:-
+let day = 2;
+
+switch (day) {           //switch statement check karta hai ke day ki value kis case se match karti hai.
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+    default:            //Agar koi case match na ho, to ye run hota hai.
+        console.log("Another day");
+}
--- index.js
+++ index.js
@@ -20,16 +20,12 @@ const country = "USA";    // can't be updated or re-declared
     console.log(pi)
 }
 
-
 var message = "Hello";
 var message = "Hi";
 console.log(message)
 
-
-
 // •	Re-declare: “Nayi baar variable banaana.”
 // •	Re-assign: “Purane variable ka value badalna.”
-
 
 // var
 var x = 1;
@@ -46,7 +42,6 @@ const z = 1;
 // const z = 2; // ❌ re-declare not allowed
 // z = 3;       // ❌ re-assign not allowed
 
-
 // DATA TYPES IN JS.
 // Primitive Data Types (Single Value, Immutable) !!UNCHANGEABLE!!
 
@@ -58,7 +53,6 @@ let city;                     // Undefined
 let bigNumber = 123456789012345678901234567890n;  // BigInt
 let id = Symbol('unique');    // Symbol
 
-
 // Non-Primitive Data Types (Multiple Values, Mutable) !!CHANGEABLE!!
 
 // Object :-
@@ -69,9 +63,27 @@ console.log(person.name);
 // Array :-
 // List of multiple values in ordered list
 let fruits = ["apple", "banana", "mango"];
+console.log(fruits[1]);
 
 // Function:-
 function greet() {
     console.log("Hello world");
 }
 greet();
+
+// let f = Symbol("Saad")
+// let g = Symbol("Saad")
+
+// var h = 10
+// var j = 10
+// console.log(h==j)
+// console.log(f==g)
+
+function greet() {
+  const a = 2;
+  
+    console.log(a);
+}
+greet()
+const a = 4
+console.log(a);
