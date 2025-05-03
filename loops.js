ae480900104f22b45debe0433a888722f7268cd6
saadkhan4 <saadshaheen1327@gmail.com>
2025-05-03 21:54:58 +0500

    learned for loop

---
+++ loops.js
@@ -0,0 +1,18 @@
+// LOOPS IN JS.
+// 1. FOR LOOP
+// 2. WHILE LOOP
+// 3. DO WHILE LOOP
+
+
+// 1. FOR LOOP
+
+// for (let i = 0; i < 3; i++){
+//     console.log("Hello world")
+// }
+
+// 2. WHILE LOOP
+let i = 1;
+while (i <= 3) {
+    console.log("Hello mars")
+    i++;
+ }
