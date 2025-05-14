aca58d9e804be9a27cee89e5d9e9e2db7aadaa71
saadkhan4 <saadshaheen1327@gmail.com>
2025-05-15 00:07:22 +0500

    table created

--- table.js
+++ table.js
@@ -20,5 +20,16 @@ const row2 = document.createElement("tr");
 tbody.appendChild(row2);
 
 const t_data = document.createElement("td");
-t_data.innerHTML = "Table data.";
-row.appendChild(t_data);
+t_data.innerHTML = "Table body.";
+row2.appendChild(t_data);
+
+const tfoot = document.createElement("tfoot");
+table.appendChild(tfoot);
+
+const row3 = document.createElement("tr");
+tfoot.appendChild(row3);
+
+const tf_data = document.createElement("td");
+tf_data.innerHTML = "Table footer";
+row3.appendChild(tf_data);
+
