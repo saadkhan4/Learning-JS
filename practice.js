4f5b6d94c9f402553fc9d6e42f948b2b102cd9f2
saadkhan4 <saadshaheen1327@gmail.com>
2025-07-10 22:22:08 +0500

    practiced of fetch

--- practice.js
+++ practice.js
@@ -385,15 +385,44 @@
 
 
 
-function bookFlight(destination, callback) {
-    setTimeout(() => {
-        console.log("Where do you want to go sir? " + destination);
-        callback();
-    }, 2000);
-}
+// function bookFlight(destination, callback) {
+//     setTimeout(() => {
+//         console.log("Where do you want to go sir? " + destination);
+//         callback();
+//     }, 2000);
+// }
 
-function confirmBooking() {
-    console.log("Here is your ticket sir.");
-}
+// function confirmBooking() {
+//     console.log("Here is your ticket sir.");
+// }
 
-bookFlight("America",confirmBooking)
+// bookFlight("America",confirmBooking)
+
+
+
+// async function getData() {
+//     try {
+//         const response = await fetch("https://dev-apis.explorebtk.com/api/v1/categories");
+//         const data = await response.json();
+//         console.log(data);
+        
+//     } catch (error) {
+//         console.log("Error",error);
+//     }
+// }
+
+// getData()
+
+// async function getData() {
+//     try {
+//         const response = await fetch("data.json");
+//         const data = await response.json();
+//         data.forEach(element => {
+//             console.log(element.grade);
+//         });
+//     } catch (error) {
+//         console.log("Error",error);
+//     }
+// }
+
+// getData()
