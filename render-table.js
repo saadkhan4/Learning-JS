2bbcf831c2f22bd1f783136e7e25bddfbbadbce3
saadkhan4 <saadshaheen1327@gmail.com>
2025-07-22 20:14:26 +0500

    fetched data via API

---
+++ render-table.js
@@ -0,0 +1,43 @@
+function renderTable(data,id) {
+    const body = document.getElementById(id);
+    const table = document.createElement("table");
+
+    body.appendChild(table);
+
+    const thead = document.createElement("thead");
+    table.appendChild(thead);
+
+    const th_row = document.createElement("tr");
+    thead.appendChild(th_row);
+
+    const tbody = document.createElement("tbody");
+    table.appendChild(tbody);
+
+
+    for (const key in data[0]) {
+        const t_heading = document.createElement("th");
+        t_heading.innerHTML = key;
+        th_row.appendChild(t_heading);
+    }
+    
+    data.forEach(element => {
+        const tb_row = document.createElement("tr");
+        tbody.appendChild(tb_row);
+        for (const key in element) {
+            const t_data = document.createElement("td");
+            t_data.innerHTML = element[key];
+            tb_row.appendChild(t_data);
+
+        }
+    });
+
+}
+
+async function getData() {
+    const response = await fetch("data.json");
+    const data = await response.json();
+    renderTable(data,"demo")
+}
+
+getData();
+
