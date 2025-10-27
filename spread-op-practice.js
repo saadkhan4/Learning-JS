201a11d3843204e9add8d8f3d7bacd8d68f8f1e1
saadkhan4 <saadshaheen1327@gmail.com>
2025-10-27 22:12:49 +0500

    practices of spread operator

---
+++ spread-op-practice.js
@@ -0,0 +1,73 @@
+// const cars = ["Audi", "BMW", "Nissan", "Ford", "Toyota"];
+
+// const automobiles = [...cars];
+
+// console.log(automobiles);
+
+// const boys = ["Tyler", "Joel", "Jim", "Mike", "Dustin"];
+// const girls = ["Nancy", "Jess", "Joyce", "Max"];
+
+// const students = [...boys, ...girls];
+
+// console.log(students);
+
+// const vegetables = ["Carrot", "Cabbage", "Tomato", "Peas", "Potato"];
+// const fruits = ["Apple", "Banana", "Orange", "Guava", "Pear"];
+
+// const list = [...vegetables, ...fruits, "Eggs", "Meat"];
+
+// console.log(list);
+
+// const user = {
+//     name: "Tim",
+//     age: 23,
+//     company : "Apple Inc"
+// }
+
+// const updatePerson = {
+//     ...user
+// }
+
+// console.log(updatePerson);
+
+// const device = {
+//     name: "Iphone 16 Pro",
+//     company: "Apple",
+//     year : 2024
+// }
+
+// const updateDevice = {
+//     ...device,
+//     city : "Apple Park CA"
+// }
+
+// console.log(updateDevice);
+
+// const info = { name: "William Byers", age: 23 };
+// const bankDetails = { type: "Premium", balance: `$${500000}` };
+
+// const result = {
+//   ...info,
+//   ...bankDetails,
+// };
+
+// console.log(result);
+
+const user = {
+  name: "Matt",
+  age: 34,
+  address: {
+    city: "Las Vegas",
+    Country: "Unites States",
+  },
+};
+
+const updatePerson = {
+    ...user, address: {
+        ...user.address,
+        city : "New York"
+    }
+}
+
+
+console.log(updatePerson);
